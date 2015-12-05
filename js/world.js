c9bb20602bb3a40718d1bdf8935c95d5c40627ae
Ollie Coulson <olliecoulson@Ollies-MBP.home>
2015-12-05 23:17:13 +0000

    Added initial basic forest graphics and experimented with collisions between character and tree trunks. Successfully done using a blocking layer for individual 16x16 tiles. Want to try to do it with the object layer.

--- js/world.js
+++ js/world.js
@@ -2,16 +2,30 @@ var WorldState = {
 
 	create: function() {
 
-		game.physics.startSystem(Phaser.Physics.ARCADE);
+		this.map = this.game.add.tilemap('BasicForest');
+
+		this.map.addTilesetImage('Grass1', 'Grass1');
+		this.map.addTilesetImage('Pines', 'Pines');
+
+		this.backgroundLayer = this.map.createLayer('Ground');
+		
+		
+
+		this.map.setCollision(249, true, 'Collisions');
+
+		game.physics.startSystem(Phaser.Physics.P2JS);
+
+		game.physics.p2.convertTilemap(this.map, "Collisions");
 
 		// Create sprite for first character
-		eve = this.game.add.sprite(game.world.centerX, game.world.centerY, 'Eve');
+		eve = this.game.add.sprite(game.world.centerX, game.world.centerY +70, 'Eve');
 		eve.anchor.setTo(0.5, 0.5);
 
 		//Give Eve physics and prevent her from moving outside the world bounds
-		game.physics.arcade.enable(eve);
+		game.physics.p2.enable(eve);
 		eve.body.collideWorldBounds = true;
 
+		eve.body.fixedRotation = true;
 
 		//Set up movement animations
 		// eve.animations.add('left', [0, 1, 2, 3], 10, true);
@@ -25,15 +39,17 @@ var WorldState = {
    	 	//Create controls to move Eve around
    	 	cursors = game.input.keyboard.createCursorKeys();
 
-
-
+   	 	this.topLayer = this.map.createLayer('Trees');
+		
+		this.topLayer.resizeWorld();
 	},
+
 
 	update: function() {
 
 		
 		if (game.input.keyboard.isDown(Phaser.Keyboard.SHIFT)) {
-			this.moveCharacter(eve, 200);
+			this.moveCharacter(eve, 150);
 		} else {
 			this.moveCharacter(eve, 100);
 		}
@@ -45,30 +61,32 @@ var WorldState = {
 	    
 	},
 	moveCharacter: function (character, speed) {
-		character.body.velocity.setTo(0,0);
+		character.body.velocity.x = 0;
+		character.body.velocity.y = 0;
+
 
 		//Link cursor keys to the movement of the character and the animation
    	 	if (cursors.left.isDown){
 	        //  Move to the west
-	        character.body.velocity.setTo(-speed, 0);	      
+	        character.body.velocity.x = -speed; 
 
 	        character.animations.play('west');
 
     	} else if (cursors.right.isDown) {
 	        //  Move to the east
-	        character.body.velocity.setTo(speed, 0);	
+	        character.body.velocity.x = speed; 
 
 	        character.animations.play('east');
 
 	    } else if (cursors.up.isDown) {
 	    	// Move to the north
-	    	character.body.velocity.setTo(0, -speed);	
+	        character.body.velocity.y = -speed;  
 
 	    	character.animations.play('north');
 
 	    } else if (cursors.down.isDown) {
 	    	// Move to the south
-			character.body.velocity.setTo(0, speed);	
+	        character.body.velocity.y = speed;
 
 	    	character.animations.play('south');
 
