aae3fa29f93de458b24f3efe4c0f439b29e2d99a
ocoulson <o_coulson@hotmail.co.uk>
2015-12-06 22:45:04 +0000

    Still can't get Eve to collide with the objects

--- js/world.js
+++ js/world.js
@@ -9,8 +9,8 @@ var WorldState = {
 
 		this.backgroundLayer = this.map.createLayer('Ground');
 		this.trunks = this.map.createLayer('Trunks');
-		
-		
+
+
 		//this.map.setCollision(249, true, 'Collisions');
 
 		game.physics.startSystem(Phaser.Physics.P2JS);
@@ -41,7 +41,7 @@ var WorldState = {
    	 	cursors = game.input.keyboard.createCursorKeys();
 
    	 	this.topLayer = this.map.createLayer('Trees');
-		
+
 		this.topLayer.resizeWorld();
 
 		this.generateCollisions(eve);
@@ -51,53 +51,55 @@ var WorldState = {
 		//Create a physics collision group which will interact with eve
 
 		blocksCollisionGroup = game.physics.p2.createCollisionGroup();
+        characterCollisionGroup = game.physics.p2.createCollisionGroup();
 
-
-		//Add make eve collide with the group
-		
-		eve.body.collidesWith =blocksCollisionGroup;
+        character.body.setCollisionGroup(characterCollisionGroup);
 
 		//Ensure that eve will still collide with world bounds.
 		game.physics.p2.updateBoundsCollisionGroup();
+        character.body.collides(blocksCollisionGroup);
 
 		// Get the blocks object from the Tiled JSON file.
-		this.blocks = this.game.add.group();
-	    this.blocks.enableBody = true;
-	    this.blocks.physicsBodyType = Phaser.Physics.P2JS;
+		blocks = this.game.add.group();
+	    blocks.enableBody = true;
+	    blocks.physicsBodyType = Phaser.Physics.P2JS;
+        game.physics.p2.enable(blocks);
 
-	    var block;    
 	    result = this.findObjectsByType('blocks', this.map, 'Collisions');
-	    result.forEach(function(element){
-	      this.createFromTiledObject(element, this.blocks);
-	    }, this);
+	    for (var i = 0; i < result.length; i++) {
+            this.createFromTiledObject(result[i], blocks);
+            blocks.getAt(i).body.setRectangle(10,10);
+            blocks.getAt(i).body.setCollisionGroup(blocksCollisionGroup);
+            blocks.getAt(i).body.collides(characterCollisionGroup);
 
-	    this.blocks.setCollisionGroup(blocksCollisionGroup);
-	    this.blocks.collides(character);
-		
+        }
+
+
+
+
 	},
 	findObjectsByType: function(type, map, layer) {
-	    var output = new Array();
+	    var output = [];
 	    map.objects[layer].forEach(function(element){
 	      if(element.properties.type === type) {
 	        //Phaser uses top left, Tiled bottom left so we have to adjust
-	        //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
-	        //so they might not be placed in the exact position as in Tiled
+
 	        element.y -= map.tileHeight;
 	        output.push(element);
-	      }      
+	      }
 	    });
     return output;
   	},
 
 	update: function() {
-		
-		
+
+
 		if (game.input.keyboard.isDown(Phaser.Keyboard.SHIFT)) {
 			this.moveCharacter(eve, 150);
 		} else {
 			this.moveCharacter(eve, 100);
 		}
-	    
+
 	},
 	moveCharacter: function (character, speed) {
 		character.body.velocity.x = 0;
@@ -107,19 +109,19 @@ var WorldState = {
 		//Link cursor keys to the movement of the character and the animation
    	 	if (cursors.left.isDown){
 	        //  Move to the west
-	        character.body.velocity.x = -speed; 
+	        character.body.velocity.x = -speed;
 
 	        character.animations.play('west');
 
     	} else if (cursors.right.isDown) {
 	        //  Move to the east
-	        character.body.velocity.x = speed; 
+	        character.body.velocity.x = speed;
 
 	        character.animations.play('east');
 
 	    } else if (cursors.up.isDown) {
 	    	// Move to the north
-	        character.body.velocity.y = -speed;  
+	        character.body.velocity.y = -speed;
 
 	    	character.animations.play('north');
 
@@ -133,7 +135,7 @@ var WorldState = {
 	    	// Stop animations if no cursor is pressed
 	    	character.animations.stop();
 	    }
-		
-	},
+
+	}
 
 };
