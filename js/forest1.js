fcd93ee56ff7c1c01d61d12df4a5504ebd262bdf
ocoulson <o_coulson@hotmail.co.uk>
2015-12-20 00:09:26 +0000

    Can now transition to second area, but not back. Also new sprite is created in new area for Eve, needs to be the same one.

--- js/forest1.js
+++ js/forest1.js
@@ -1,50 +1,21 @@
 /**
- * Created by ocouls01 on 13/12/2015.
+ * Created by olliecoulson on 19/12/2015.
  */
-
-var EntranceState = {
-
+var Forest1State = {
 
     create: function() {
-        //Set the JSON Entrance as the map of the area
-        this.map = this.game.add.tilemap('Entrance');
+        // Set the map object
+        this.map = this.game.add.tilemap('Forest1');
         //Make the world bounds as big as the tilemap
-        game.world.setBounds(0, 0, 640, 800);
+        game.world.setBounds(0, 0, 1920, 800);
 
         //Set the tileset image
         this.map.addTilesetImage('TilesheetBiodome', 'TilesheetBiodome');
 
         //Create the layers from the Tiled tilemap
         this.BaseLayer = this.map.createLayer('BaseLayer');
-        this.Beach = this.map.createLayer('Beach');
+        this.Forest = this.map.createLayer('Forest');
 
-        //Create and animate the shore
-        //Create a group of sprites to be the south shore, and animate them
-        shore = game.add.group();
-        for (var i = 2; i < 40; i++) {
-            shorePiece =  shore.create(i*16,720, 'shoreBottom');
-
-        }
-
-        shore.callAll('animations.add', 'animations', 'wave',
-            [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], 10, true);
-        shore.callAll('play', null, 'wave');
-
-        //Create a group to animate the secondary wave sprites
-
-        wave = game.add.group();
-        for (var i = 1; i < 40; i ++) {
-            wavePiece = wave.create(i*16, 752, 'waveBottom');
-        }
-
-        wave.callAll('animations.add', 'animations', 'wave2', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], 10, true);
-        wave.callAll('play', null, 'wave2');
-
-
-        // add the rest of the layers (rendered in this order)
-        this.map.createLayer('BeachShadows');
-        this.BeachFeatures = this.map.createLayer('BeachFeatures');
-        this.map.createLayer('Beach2Forest');
         this.Forest = this.map.createLayer('Forest');
         //Ridges before features and trees so they render on top.
         this.map.createLayer('Ridges');
@@ -66,15 +37,10 @@ var EntranceState = {
         game.physics.p2.convertTilemap(this.map, "Trunks");
 
 
-        //Create a sign sprite for Player to interact with.
-        sign = game.add.sprite(48, 80, 'Sign');
-        game.physics.p2.enable(sign);
-        sign.body.static = true;
-        sign.body.physicsBodyType = Phaser.Physics.P2JS;
 
+        //Create a new eve for now, but will want to pass the previous sprite object through
 
-        // Create sprite for first character
-        eve = this.game.add.sprite(224, 672, 'Eve');
+        eve = this.game.add.sprite(1872, 512, 'Eve');
         eve.anchor.setTo(0.5, 0.5);
 
         //Give Eve physics and prevent her from moving outside the world bounds
@@ -87,8 +53,6 @@ var EntranceState = {
 
         eve.body.fixedRotation = true;
 
-        //Set up movement animations
-
         eve.animations.add('west', [8, 9, 8, 10], 10, true);
         eve.animations.add('east', [11, 12, 11, 13], 10, true);
         eve.animations.add('north', [3, 4, 3, 5], 10, true);
@@ -100,42 +64,16 @@ var EntranceState = {
         //Set the camera to follow Eve
         game.camera.follow(eve);
 
-        //Create 'deadzone' for camera so it only follows eve
-        //when she enters the edges of the screen.
-        game.camera.deadzone = new Phaser.Rectangle(128, 128, 384, 144);
-
-
-        //Create the trees after Eve so she passes behind them.
-        this.map.createLayer('Trees');
-
-
+        //Graphics rendered on top of character
+        this.Trees = this.map.createLayer('Trees');
     },
 
     update: function() {
-
-
-        spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-        spacebar.onDown.add(this.printPosition, this);
-
-
-        if ((eve.position.x <10) && ((eve.position.y > 80) && (eve.position.y < 113))) {
-            this.game.state.start('loadForest1');
-        }
-
-
-
-        // log the position to the console
         if (game.input.keyboard.isDown(Phaser.Keyboard.SHIFT)) {
             this.moveCharacter(eve, 150);
         } else {
             this.moveCharacter(eve, 100);
         }
-
-    },
-
-    printPosition: function() {
-        console.log(eve.position.toString()) ;
     },
 
     moveCharacter: function (character, speed) {
--- js/game.js
+++ js/game.js
@@ -7,6 +7,9 @@ game.state.add('boot', BootState);
 game.state.add('loadTitle', LoadTitleState);
 game.state.add('loadEntrance', LoadEntranceState);
 game.state.add('entrance', EntranceState);
+game.state.add('loadForest1', LoadForest1State);
+game.state.add('forest1', Forest1State);
+
 
 
 //game.state.add('loadWorld', LoadWorldState);
